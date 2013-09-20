282ade348fed71605402af15feb46eb7458dc576
Matthew Kastor <matthewkastor@gmail.com>
2013-09-20 07:31:55 -0400

    wrote test

--- specs/dir-stats-sync.Spec.js
+++ specs/dir-stats-sync.Spec.js
@@ -1,6 +1,41 @@
-var dirStatsSync = require('../src/dir-stats-sync.js');
-var fs = require('fs');
-var path = require('path');
-var specPath = path.resolve(__dirname, '../browser/tests/dir-stats-sync.test.js');
-var specCode = fs.readFileSync(specPath, "utf8");
-eval(specCode);
+describe('dir-stat-sync', function () {
+    var fs = require('fs');
+    var path = require('path');
+    var dirStatsSync = require('../src/dir-stats-sync.js');
+    var dir = path.resolve(__dirname, 'testData');
+    
+    function fixture () {
+        var data = {
+            stat : {},
+            lstat : {}
+        };
+        
+        function lstatCallback (item, stat, dir) {
+            data.lstat[item] = stat;
+        }
+        dirStatsSync.lstatDir(dir, lstatCallback);
+        
+        function statCallback (item, stat, dir) {
+            data.stat[item] = stat;
+        }
+        dirStatsSync.statDir(dir, statCallback);
+        return data;
+    }
+    
+    var data = fixture();
+    
+    describe('statDir', function () {
+        it('should stat each item in a directory', function () {
+            expect(data.stat.folder.isDirectory()).toEqual(true);
+            expect(data.stat.textFile.isFile()).toEqual(true);
+        });
+    });
+    
+    describe('lstatDir', function () {
+        it('should stat each item in a directory', function () {
+            expect(data.lstat.folder.isDirectory()).toEqual(true);
+            expect(data.lstat.textFile.isFile()).toEqual(true);
+        });
+    });
+    
+});
